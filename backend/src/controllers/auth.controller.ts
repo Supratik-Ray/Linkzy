c442e61400fe11422b62de826a96eeb0e6a6f409
Supratik <raysupratik8@gmail.com>
2026-05-05 09:37:26 +0530

    feat: completed the auth controller functions

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,3 +1,77 @@
-export async function signup() {}
+import bcrypt from "bcryptjs";
+import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import User from "../models/User.ts";
 
-export async function login() {}
+export async function signup(req: Request, res: Response) {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: "Please provide all fields!",
+    });
+  }
+
+  //hash password
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //create user and if email duplicate then return duplicate email error
+
+  try {
+    const user = await User.create({ name, email, passwordHash });
+
+    const payload = { id: user.id, name: user.name, email: user.email };
+
+    //create token
+    const token = jwt.sign(payload.id, process.env.JWT_SECRET!);
+
+    return res.status(StatusCodes.CREATED).json({
+      message: "successfully signed up!",
+      token,
+      user: payload,
+    });
+  } catch (err) {
+    const error = err as { code: number };
+
+    if (error.code === 11000) {
+      return res.status(StatusCodes.CONFLICT).json({
+        message: "User with this email already exists!",
+      });
+    }
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  //check if the user exists
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "Invalid email or password!",
+    });
+  }
+
+  //check if password match
+  const isMatch = await bcrypt.compare(password, user.passwordHash);
+  if (!isMatch) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "Invalid email or password!",
+    });
+  }
+
+  const payload = { id: user.id, name: user.name, email: user.email };
+
+  //create token
+  const token = jwt.sign(payload.id, process.env.JWT_SECRET!);
+
+  return res.status(StatusCodes.OK).json({
+    message: "logged in successfully!",
+    user: payload,
+    token,
+  });
+}
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      minLength: 2,
+    },
     email: {
       type: String,
       required: true,
