7b014ee475e6c972d632113baa921787a7dc1623
Supratik <raysupratik8@gmail.com>
2026-05-05 18:23:18 +0530

    feat: created shortenLink controller function

--- backend/src/controllers/link.controller.ts
+++ backend/src/controllers/link.controller.ts
@@ -1,9 +1,36 @@
 import type { Request, Response } from "express";
-import { BadRequestError } from "../errors/index.ts";
+import { AuthError, BadRequestError, ServerError } from "../errors/index.ts";
+import { generateCode } from "../utils/generateCode.ts";
+import Link from "../models/Link.ts";
+import { StatusCodes } from "http-status-codes";
 
 export async function shortenLink(req: Request, res: Response) {
-  const { originalLink } = req.body;
+  if (!req.user) {
+    throw new AuthError("You are unauthorized to access this page!");
+  }
 
-  if (!originalLink)
+  const { originalUrl } = req.body;
+
+  if (!originalUrl)
     throw new BadRequestError("please provide the original link");
+
+  const shortCode = generateCode(6);
+
+  try {
+    await Link.create({
+      originalUrl,
+      shortCode,
+      user: req.user.id,
+    });
+
+    return res.status(StatusCodes.CREATED).json({
+      success: true,
+      data: {
+        originalUrl,
+        shortCode,
+      },
+    });
+  } catch (error) {
+    throw new ServerError("failed to shorten link!");
+  }
 }
